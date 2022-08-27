b5cb3b8f7dce580b4a4c362a6d0ffb046b49a663
zhwuyuhehe <47172382+zhwuyuhehe@users.noreply.github.com>
2022-08-28 01:34:40 +0800

    Update conn.php

--- functions/conn.php
+++ functions/conn.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new mysqli("8.142.76.205", "root", "ZHANGhao2002", "sky");
+$conn = new mysqli("xxx.xxx.xxx.xxx", "root", "ZHANGhao2002", "sky");
 //链接数据库的配置在这里，如需修改IP直接修改这一个地方的即可。
 //服务器到期了呜呜呜，本地写个json先展示题目吧~
